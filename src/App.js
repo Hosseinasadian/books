b154489d9dac6f717ca39558f1d98ca89986e6bd
hossein <hosseinasadian442@gmail.com>
2025-12-04 23:43:59 +0330

    fix deploy bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import BookList from './pages/BookList';
 import BookDetail from './pages/BookDetail';
 import './App.css';
@@ -44,7 +44,7 @@ function App() {
   }
 
   return (
-    <Router basename="/books">
+    <Router>
       <div className="app">
         <Routes>
           <Route 
